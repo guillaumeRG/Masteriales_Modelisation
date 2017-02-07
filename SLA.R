113aa8a2bb3412d91ec013be308eb9eebef89f43
Guest <guest-Rpbssl@ib102-20.miage.info.univ-evry.fr>
2017-02-07 12:00:32 +0100

    real first commit

---
+++ SLA.R
@@ -0,0 +1,25 @@
+negociateFunction()
+
+negociateFunction <- function(){
+  round<-1
+  initFunction()
+  print(P0)
+  return(0)
+}
+
+initFunction<- function(){
+  P0<<-0
+  Q0<<-0
+  Qmax<<-0
+  Pmax<<-0
+  Qmin<<-0
+  Pmin<<-0
+  Ssp_P<<-0
+  Ssp_Q<<-0
+  Sc_P<<-0
+  Sc_Q<<-0
+  DELTA<<-0
+  delta<<-0
+}
+
+
